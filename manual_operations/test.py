7ab915e4837b5daa8c942e6ec409f1a191303077
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-11-27 21:56:03 +0700

    Update reachable test code

--- manual_operations/test.py
+++ manual_operations/test.py
@@ -1,8 +1,7 @@
+import asyncio
 import os
 import vertexai
 from vertexai import agent_engines
-
-MODEL = "gemini-2.5-flash-lite"
 
 GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
 GOOGLE_CLOUD_LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
@@ -13,7 +12,29 @@ vertexai.init(project=GOOGLE_CLOUD_PROJECT, location=GOOGLE_CLOUD_LOCATION)
 agents_list = list(agent_engines.list())
 if agents_list:
     remote_agent = agents_list[0]  # Get the first (most recent) agent
-    client = agent_engines
     print(f"✅ Connected to deployed agent: {remote_agent.resource_name}")
 else:
     print("❌ No agents found. Please deploy first.")
+
+
+client = vertexai.Client(
+    project=GOOGLE_CLOUD_PROJECT,
+    location=GOOGLE_CLOUD_LOCATION,
+)
+
+adk_app = client.agent_engines.get(name=remote_agent.resource_name)
+
+
+async def main():
+    user_id = "user_123123123"
+    session = await adk_app.async_create_session(user_id=user_id)
+    print(session)
+    async for event in adk_app.async_stream_query(
+        user_id=user_id,
+        session_id=session["id"],
+        message="Hello",
+    ):
+        print(event)
+
+
+asyncio.run(main())
