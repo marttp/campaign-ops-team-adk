5e659254a3551dddca53156ed97bd828751c6b50
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-11-30 21:12:33 +0700

    feat: enable continuous interactive chat in test script with user input and graceful exit

--- manual_operations/test.py
+++ manual_operations/test.py
@@ -28,17 +28,22 @@ adk_app = client.agent_engines.get(name=remote_agent.resource_name)
 async def main():
     user_id = "user_123123123"
     session = await adk_app.async_create_session(user_id=user_id)
-    # print(session)
-    # {'appName': '8208879046184927232', 'userId': 'user_123123123', 'state': {}, 'events': [], 'id': '8586600048110862336', 'lastUpdateTime': 1764279302.661221}
-    async for event in adk_app.async_stream_query(
-        user_id=user_id,
-        session_id=session["id"],
-        message="Hello",
-    ):
+    # Run until the user stops the process (Ctrl+C)
+    while True:
         try:
-            print(event["content"]["parts"][0]["text"])
-        except (KeyError, IndexError, TypeError):
-            print(event)
+            input_message = input("User: ")
+            async for event in adk_app.async_stream_query(
+                user_id=user_id,
+                session_id=session["id"],
+                message=input_message,
+            ):
+                try:
+                    print(event["content"]["parts"][0]["text"])
+                except (KeyError, IndexError, TypeError):
+                    print(event)
+        except KeyboardInterrupt:
+            print("Stopping stream...")
+            break
 
 
 asyncio.run(main())
